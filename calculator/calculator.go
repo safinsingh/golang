6005e3a79b9d24ceb42764b6fe073a5a036e91b2
safinsingh <safin.singh@gmail.com>
2020-05-13 10:39:04 -0700

    Add walrii to calculator

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var done bool
 	done = false
-	for done == false {
+	for !done {
 		var text string
 
 		fmt.Print("Enter calculation (A/S/M/D): ")
@@ -24,11 +24,8 @@ func main() {
 			fmt.Print("Enter your second number: ")
 			fmt.Scanln(&num2)
 
-			var sum float32
-			var sumString string
-
-			sum = num1 + num2
-			sumString = fmt.Sprintf("%f", sum)
+			sum := num1 + num2
+			sumString := fmt.Sprintf("%f", sum)
 			fmt.Print("Your sum is: " + sumString + "\n")
 		} else if strings.TrimRight(text, "\n") == "D" {
 			var num1 float32
@@ -40,11 +37,8 @@ func main() {
 			fmt.Print("Enter your second number: ")
 			fmt.Scanln(&num2)
 
-			var quotient float32
-			var quotientString string
-
-			quotient = num1 / num2
-			quotientString = fmt.Sprintf("%f", quotient)
+			quotient := num1 / num2
+			quotientString := fmt.Sprintf("%f", quotient)
 			fmt.Print("Your quotient is: " + quotientString + "\n")
 		} else if strings.TrimRight(text, "\n") == "M" {
 			var num1 float32
@@ -56,11 +50,8 @@ func main() {
 			fmt.Print("Enter your second number: ")
 			fmt.Scanln(&num2)
 
-			var product float32
-			var productString string
-
-			product = num1 * num2
-			productString = fmt.Sprintf("%f", product)
+			product := num1 * num2
+			productString := fmt.Sprintf("%f", product)
 			fmt.Print("Your product is: " + productString + "\n")
 		} else if strings.TrimRight(text, "\n") == "S" {
 			var num1 float32
@@ -72,11 +63,8 @@ func main() {
 			fmt.Print("Enter your second number: ")
 			fmt.Scanln(&num2)
 
-			var diff float32
-			var diffString string
-
-			diff = num1 - num2
-			diffString = fmt.Sprintf("%f", diff)
+			diff := num1 - num2
+			diffString := fmt.Sprintf("%f", diff)
 			fmt.Print("Your product is: " + diffString + "\n")
 		} else {
 			fmt.Print("Hmm... I didn't recognize that, let's try again!\n")
